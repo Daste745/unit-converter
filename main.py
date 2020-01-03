b264f42b1914a120f971bae84d0090827b2efd90
Stefankar1000 <stefankar1000@gmail.com>
2020-01-03 21:03:04 +0100

    Add temperature convertion.

--- main.py
+++ main.py
@@ -34,9 +34,27 @@ def convert(a: str, b: str, value: Union[int, float]) -> Union[int, float]:
     base_unit_ratio = conversion_table[unit_type(a)]["conversions"][a]
     target_unit_ratio = conversion_table[unit_type(b)]["conversions"][b]
 
-    # TODO: Make Temperature conversion
+    # Temperature gets its own section, because it includes
+    # addition/subtraction and a specific ratio for fahrenheit
     if unit_type(a) == "temperature":
-        raise NotImplementedError("Temperature conversion not yet implemented")
+        to_celsius: int
+        if a == "celsius":
+            to_celsius = value
+        elif a == "fahrenheit":
+            # [C] = ([F] − ratio) * (5/9)
+            to_celsius = (value - base_unit_ratio) * (5/9)
+        elif a == "kelvin":
+            # [C] = [K] − ratio
+            to_celsius = value - base_unit_ratio
+
+        if b == "celsius":
+            return to_celsius
+        elif b == "fahrenheit":
+            # [F] = [C] * (9/5) + ratio
+            return to_celsius * 1.8 + target_unit_ratio
+        elif b == "kelvin":
+            # [K] = [C] + ratio
+            return to_celsius + target_unit_ratio
 
     return value / base_unit_ratio * target_unit_ratio
 
